16f96930124e61ff1150734702874a77fa3cd25b
Louis Pilfold <louis@lpil.uk>
2015-02-15 12:35:11 +0000

    We have chat

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/time', function(req, res) {
   res.send(
     'It is ' + new Date().toGMTString() + '!'
   );
+});
+
+io.on('connection', function(socket) {
+  io.emit('chat message', 'User connected');
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('chat message', 'User disconnected');
+  });
 });
 
 http.listen(3000, function(){
